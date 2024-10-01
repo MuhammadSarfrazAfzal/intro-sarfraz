1fe16b798007dc7bdc88d2fa95e05e76085c8fd0
MuhammadSarfrazAfzal <sarfrazafzal790@gmail.com>
2024-10-01 19:52:45 +0500

    Made into component

---
+++ src/assets/components/intro.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./intro.css"
+const Intro = () => {
+  return (
+    <>
+        <div className="about-me-container">
+      <div className="about-me-content">
+        <h1>Hello, I'm Muhammad Sarfraz Afzal</h1>
+        <p>
+          I am 21 years old and currently working on improving my skills as a 
+          React.js developer. I’ve learned frontend development in React.js, 
+          and I’m actively looking for a job in this field. 
+        </p>
+        <p>
+          Currently, I'm working on a project called <strong>Travel Advisor</strong>, 
+          which helps users plan their travels effectively.
+        </p>
+        <p>
+          Alongside my technical growth, I’m also focusing on enhancing my English 
+          speaking skills to communicate fluently in job interviews and everyday conversations. 
+          I practice speaking English daily at 8 p.m. to get more comfortable.
+        </p>
+        <p>
+          I’m also following a weight gain diet plan to reach my health goals.
+        </p>
+        <p>Thank you for visiting my page!</p>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Intro
